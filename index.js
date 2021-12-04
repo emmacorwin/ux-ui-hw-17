d86af2e0615e48d1bcce879ae9f0ad8f86e8ff0f
emmacorwin <emmalucorwin@gmail.com>
2021-12-04 10:18:11 -0700

    update

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("Your index is loading correctly");
+
+$(".name").on("click",function(){
+    console.log("IT WORKS")   
+}
+);
+
+
+$(".name").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+
+  $("#ux").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+  $("#graphic").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+  $("#painting").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+  $("#about").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "white");
+  }
+  );
+
+
+  
